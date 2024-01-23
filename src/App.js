f779f397b8058bbcf49febb019bee1b4ceb9965d
Liliane-Top <hallo@lilianetop.nl>
2024-01-23 22:59:03 +0100

    implemented the business logic, all works well except for the reset does not clear the input field nor the default values of the percentages needs to be fixed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,79 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>Liliane gaat beginnen..</div>;
+  const [amount, setAmount] = useState(0);
+  const [myPercentage, setMyPercentage] = useState(0);
+  const [friendPercentage, setMyFriendPercentage] = useState(0);
+
+  function handleReset() {
+    setAmount(0);
+    setMyPercentage(0);
+    setMyFriendPercentage(0);
+  }
+
+  return (
+    <div>
+      <Bill onSetAmount={setAmount} />
+      <Percentage onSetPercentage={setMyPercentage}>
+        How did you like the service?
+      </Percentage>
+      <Percentage onSetPercentage={setMyFriendPercentage}>
+        How did your friend like the service?
+      </Percentage>
+      <OutPut
+        amount={amount}
+        myPercentage={myPercentage}
+        friendPercentage={friendPercentage}
+      />
+      <Reset onHandleClick={handleReset} />
+    </div>
+  );
+}
+
+function Bill({ onSetAmount }) {
+  return (
+    <div>
+      <h3>How much was the bill?</h3>
+      <input
+        type="text"
+        placeholder="Insert amount"
+        onChange={(e) => onSetAmount(e.target.value)}
+      />
+    </div>
+  );
+}
+
+function Percentage({ onSetPercentage, children }) {
+  return (
+    <div>
+      <h3>{children}</h3>
+      <select onChange={(e) => onSetPercentage(e.target.value)}>
+        <option value={0}>Dissatisfied(0%)</option>
+        <option value={5}>It was okay(5%)</option>
+        <option value={10}>It was good(10%)</option>
+        <option value={20}>Absolutely amazing(20%)</option>
+      </select>
+    </div>
+  );
+}
+
+function OutPut({ amount, myPercentage, friendPercentage }) {
+  const tip =
+    ((Number(myPercentage) + Number(friendPercentage)) * amount) / 100;
+  const totalAmount = Number(amount) + Number(tip);
+  return (
+    <div>
+      <h2>
+        You pay ${totalAmount} (${amount} + ${tip} tip)
+      </h2>
+    </div>
+  );
+}
+
+function Reset({ onHandleClick }) {
+  return (
+    <button type="reset" onClick={onHandleClick}>
+      Reset
+    </button>
+  );
 }
